79a9d280314b0c98f53bbd4d36286343033266e4
Crystal Mejia <34174452+crysmej04@users.noreply.github.com>
2018-03-07 01:55:01 -0600

    Add files via upload

---
+++ deck_csharp/Program.cs
@@ -0,0 +1,45 @@
+﻿//continuing to work on...
+using System;
+
+namespace deck_csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             Card jon = new Card("jon", "black", 1);
+             Console.WriteLine(jon.val);
+             Deck bob = new Deck().reset();
+             Console.WriteLine(bob.cards.Count);
+            Deck troy = new Deck();
+            Player bill = new Player("Rick");
+            Console.WriteLine(troy.cards.Count);
+
+            bill.draw(troy);
+            Console.WriteLine(bill.hand.Count);
+            bill.draw(troy);
+            bill.draw(troy);
+            bill.draw(troy);
+            bill.draw(troy);
+            bill.draw(troy);
+            bill.draw(troy);
+            bill.draw(troy);
+            bill.draw(troy);
+            bill.draw(troy);
+            bill.draw(troy);
+            bill.draw(troy);
+            bill.draw(troy);
+            bill.draw(troy);
+            bill.draw(troy);
+            bill.draw(troy);
+            bill.draw(troy);
+            bill.draw(troy);
+            bill.draw(troy);
+            bill.draw(troy);
+            Console.WriteLine(bill.hand.Count);
+            bill.discard(4);
+            Console.WriteLine(bill.hand.Count);
+            
+        }
+    }
+}
