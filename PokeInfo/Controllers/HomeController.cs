db6c0839f9377bf943e133724844a6a8f18098f9
Crystal Mejia <34174452+crysmej04@users.noreply.github.com>
2018-03-08 15:52:18 -0600

    Add files via upload

---
+++ PokeInfo/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+
+namespace pokeinfo.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: /Home/
+        [HttpGet]
+        [Route("")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: /pokemon/pokeid
+        [HttpGet]
+        [Route("pokemon/{pokeid}")]
+        public IActionResult QueryPoke(int pokeid)
+        {
+            var CurrPokemon = new Pokemon();
+            
+            WebRequest.GetPokemonDataAsync(pokeid, ApiResponse =>
+                {
+                    CurrPokemon = ApiResponse;
+                    Console.WriteLine("========================================================CurrPokemon: "+CurrPokemon);
+                }
+            ).Wait();
+            Console.WriteLine("Waiting ====== Waiting =========== Waiting ========= Waiting ==========");
+            ViewBag.Pokemon = CurrPokemon;
+            return View("Index");
+        }    
+    }
+}
